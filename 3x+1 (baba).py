213a5ba174887f56e9ab67583fb92f16edfb63ff
Ahmad Danial <danial@razham.com>
2021-09-14 07:17:50 +0800

    another test, just to be sure

--- 3x+1 (baba).py
+++ 3x+1 (baba).py
@@ -1,1 +1,12 @@
-adsadad
+""" 
+x = "123452500" # True  
+if x.isnumeric() == True:  
+    print("Numeric")  
+else:  
+    print("Not numeric")  
+x2 = "123-4525-00" # False  
+if x2.isnumeric() == True:  
+    print("Numeric")  
+else:  
+    print("Not numeric")   """
+
