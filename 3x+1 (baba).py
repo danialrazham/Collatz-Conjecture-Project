4a00f837e9b3fe98d3f32a2a07e317a69d80ef4f
Ahmad Danial <danial@razham.com>
2021-09-15 13:56:06 +0800

    Level 1 complete, a little messy but the collatz works pretty much

--- 3x+1 (baba).py
+++ 3x+1 (baba).py
@@ -1,12 +1,35 @@
-""" 
-x = "123452500" # True  
-if x.isnumeric() == True:  
-    print("Numeric")  
-else:  
-    print("Not numeric")  
-x2 = "123-4525-00" # False  
-if x2.isnumeric() == True:  
-    print("Numeric")  
-else:  
-    print("Not numeric")   """
+
+
+""" count = 0
+
+def collatz(i):
+    if (i % 2) != 0:
+        i = 3*i + 1
+        print(i)
+    else:
+        i = i/2
+        print(i)
+    while i != 2:
+        if (i % 2) != 0:
+            i = 3*i + 1
+            print(i)
+        else:
+            i = i/2
+            print(i)
+    print(f'The collatz function ends at {i}'.format(i))
+    return i
+
+for i in range(1,10):
+    collatz(i)
+
+ """
+
+ 
+
+
+
+
+
+
+
 
