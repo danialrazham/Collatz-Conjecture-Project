057e64b51a85d085e8ab5e3a559d5ab10e00f1a4
Ahmad Danial <danial@razham.com>
2021-09-18 16:08:25 +0800

    seperated the functions but cant get them to work properly hmmm

--- 3x+1 (baba).py
+++ 3x+1 (baba).py
@@ -1,6 +1,6 @@
 
-def collatz(i):
-    c = 0
+""" def collatz_whole(i):
+    global c
     if (i % 2) != 0:
         i = 3*i + 1
         c += 1
@@ -19,18 +19,23 @@ def collatz(i):
             c += 1
             print(i)
     print(f'The collatz function ends at {i} after {c} iterations')
-    return i
-    
-for i in range(1,10):
+    return i """
+  ###################################################################################################################################  
+
+
+""" for i in range(1,10):
     print(f'collatz for {i}'.format(i))
-    collatz(i)  
+    collatz_whole(i)  
+ """
 
+c = 0
+def counter():
+    global c
+    c += 1
+    return c
+    
 
-
-
-
-
-""" def collatz(i):
+def collatz(i):
     if (i % 2) != 0:
         i = 3*i + 1
         print(i)
@@ -39,10 +44,15 @@ for i in range(1,10):
         print(i)
     return i
 
+        
 for i in range(1,10):
-    print(f'collatz for {i}'.format(i))
+    print(f'Collatz for {i}'.format(i))
     collatz(i)
-    if i != 2:
+    counter()
+    while i != 2:
         collatz(i)
+        counter()
     else:
-        print(f'collatz has ended at {i}') """
+        print(f'collatz ends at {i} after {c} iterations')
+    
+
