2d58aa421ae0134c6e00f62eb3605b19aabd665b
Ahmad Danial <danial@razham.com>
2021-09-18 15:41:00 +0800

    Level 2 completed, counter works as intended

--- 3x+1 (baba).py
+++ 3x+1 (baba).py
@@ -1,35 +1,48 @@
 
-
-""" count = 0
-
 def collatz(i):
+    c = 0
+    if (i % 2) != 0:
+        i = 3*i + 1
+        c += 1
+        print(i)
+    else:
+        i = i/2
+        print(i)
+        c += 1
+    while i != 2:
+        if (i % 2) != 0:
+            i = 3*i + 1
+            c += 1
+            print(i)
+        else:
+            i = i/2
+            c += 1
+            print(i)
+    print(f'The collatz function ends at {i} after {c} iterations')
+    return i
+    
+for i in range(1,10):
+    print(f'collatz for {i}'.format(i))
+    collatz(i)  
+
+
+
+
+
+
+""" def collatz(i):
     if (i % 2) != 0:
         i = 3*i + 1
         print(i)
     else:
         i = i/2
         print(i)
-    while i != 2:
-        if (i % 2) != 0:
-            i = 3*i + 1
-            print(i)
-        else:
-            i = i/2
-            print(i)
-    print(f'The collatz function ends at {i}'.format(i))
     return i
 
 for i in range(1,10):
+    print(f'collatz for {i}'.format(i))
     collatz(i)
-
- """
-
- 
-
-
-
-
-
-
-
-
+    if i != 2:
+        collatz(i)
+    else:
+        print(f'collatz has ended at {i}') """
