16cfaa1b55bb164e4e11c5713440bc67515998c8
Ahmad Danial <danial@razham.com>
2021-09-19 11:57:42 +0800

    This thing draws graphs now (this is iterations graph)

--- 3x+1 (baba).py
+++ 3x+1 (baba).py
@@ -1,9 +1,13 @@
+import matplotlib.pyplot as graph
+
+x = []
+y = []
 
 def counter():
     global c
     c += 1
     return c
-    
+
 
 def collatz(i):
     if (i % 2) != 0:
@@ -19,17 +23,30 @@ def maxCheck():
     if currentCollatz > max:
         max = currentCollatz
     return max
-     
-for i in range(30,51):
+
+def drawGraph(x,y):
+    global c
+    global i
+    graph.plot(x,y,color='blue', linewidth=2,marker="o",markersize=5)
+    graph.xlabel("Input into Collatz")
+    graph.ylabel("Number of Iterations")
+    graph.title("Number of Iterations for each Collatz")
+    graph.ylim(0,20)
+    graph.xlim(0,10)
+    graph.show()
+
+
+for i in range(1,11):
     max = 0
     c = 1
+    x.append(i)
     print(f'Collatz for {i}'.format(i))
     currentCollatz = collatz(i)
     while currentCollatz != 1:
         currentCollatz = collatz(currentCollatz)
         max = maxCheck()
         counter()
+    y.append(c)
     print(f'Collatz has ended after {c} iterations with a maximum value of {max}')
-      
 
-    
+drawGraph(x,y)
