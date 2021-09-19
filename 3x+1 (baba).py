ad2629c0f2b23f6d7d78613b3e1c567f9cd68557
Ahmad Danial <danial@razham.com>
2021-09-19 11:09:31 +0800

    commiting to fix something?

--- 3x+1 (baba).py
+++ 3x+1 (baba).py
@@ -30,6 +30,11 @@ for i in range(1,10):
 
 
 
+""" def counter():
+    global c
+    c += 1
+    return c """
+    
 
 
 """ def collatz(i):
@@ -40,6 +45,21 @@ for i in range(1,10):
         i = i/2
         print(i)
     return i
+
+def maxCheck():
+    
+     
+for i in range(1,11):
+    c = 1
+    print(f'Collatz for {i}'.format(i))
+    i = collatz(i)
+    while i != 1:
+        i = collatz(i)
+        counter()
+    print(f'Collatz has ended at {i} after {c} iterations')
+      
+
+    
 
 for i in range(1,10):
     print(f'collatz for {i}'.format(i))
